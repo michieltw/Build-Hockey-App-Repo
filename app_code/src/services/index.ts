632f9ae00a3c99e2a121def5eff1a1e21943144b
michieltw <michielwaalboer@gmail.com>
2026-08-15 22:25:08 +0200

    Delete app_code directory

--- app_code/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DatabaseService';
-export * from './PersonService';
-export * from './TeamService';
-export * from './LeagueService';
-export * from './MatchService';
-export * from './SocialService';
